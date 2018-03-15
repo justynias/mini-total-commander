3126d0a7caa367437f0a94b9aa08a879cf3f9dca
justynias <justynakozik1@gmail.com>
2018-03-15 19:00:13 +0100

    laboratory version

---
+++ TotalCommander/TotalCommander/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TotalCommander
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            miniTCPanel1.CurrentPath=@"c:\";
+            //DriveInfo.GetDrives.Name
+            //    Directory.GetFiles
+            //    Path.GetDirectoryName
+        
+
+        }
+
+
+    }
+}
---
+++ TotalCommander/TotalCommander/MiniTCPanel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace TotalCommander
+{
+    public partial class MiniTCPanel : UserControl
+    {
+        public MiniTCPanel()
+        {
+            InitializeComponent();
+        }
+        public string CurrentPath
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                textBox1.Text = value;
+
+            }
+        }
+
+
+
+        private void comboBoxLoad(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                comboBox1.Items.Add(d.Name);
+                if (d.IsReady)
+                {
+                    
+                }
+
+            }
+        }
+
+        private void setTextBoxPath(object sender, EventArgs e)
+        {
+          ComboBox combobox = sender as ComboBox;
+            textBox1.Text = combobox.SelectedItem.ToString();
+        }
+    }
+}
